70de29599944bf35157d453a5abc678d77b656a1
Boris <bobabonanadz@mail.ru>
2020-06-02 00:28:16 +0300

    Slider & stick header button menu

--- resources/js/homepage-slider/slider.js
+++ resources/js/homepage-slider/slider.js
@@ -1,22 +1,13 @@
 (() => {
-// variables
+    // variables
     const prev = document.querySelector('.prev');
     const next = document.querySelector('.next');
     const sliderItems = document.querySelectorAll('.slider__item');
-    //const sliderDots = document.querySelector('.slider__dots');
     const sliderContainer = document.querySelector('.slider');
 
     let index = 0; // Current position
     let pause = 5000;
     let interval;
-    /* ============= */
-    /* let dots = '';
-    sliderItems.forEach((item, i) => {
-        dots += `<span class="slider__dots--dot${(index === i) ? ' active' : ''}"></span>`;
-    });
-    sliderDots.innerHTML = dots;
-    const dotsBlock = sliderDots.querySelectorAll('.slider__dots--dot'); */
-    /* ============ */
 
     /**
      * Stop slider
@@ -44,34 +35,25 @@
     const activeSlide = i => {
         addClassActive(sliderItems, i);
     }
-    /**
-     *
-     * @param i
-     */
-    /* const activeDot = i => {
-        addClassActive(dotsBlock, i);
-    }*/
+
     /**
      *
      * @param i
      */
     const prepareCurrentSlide = i => {
         activeSlide(i);
-        //activeDot(i);
     }
     /**
      * Next
      */
     const getNext = () => {
-        interval = setInterval(() => {
-            if ((sliderItems.length - 1) === index) {
-                index = 0;
-                prepareCurrentSlide(index);
-            } else {
-                index++;
-                prepareCurrentSlide(index);
-            }
-        }, pause);
+        if ((sliderItems.length - 1) === index) {
+            index = 0;
+            prepareCurrentSlide(index);
+        } else {
+            index++;
+            prepareCurrentSlide(index);
+        }
     }
     /**
      * Prev
@@ -85,23 +67,20 @@
             prepareCurrentSlide(index);
         }
     }
-
     /**
-     * Dots click event
+     * Start
      */
-    /* dotsBlock.forEach((item, i) => {
-        item.addEventListener('click', handle => {
-            //index = i;
-            prepareCurrentSlide(i);
-            stopSlider();
-        });
-    });*/
+    const start = () => {
+        interval = setInterval(() => {
+            getNext();
+        }, pause);
+    }
 
     next.addEventListener('click', getNext);
     prev.addEventListener('click', getPrev);
 
     sliderContainer.addEventListener("mouseenter", stopSlider);
-    sliderContainer.addEventListener("mouseleave", getNext);
+    sliderContainer.addEventListener("mouseleave", start);
 
-    getNext();
+    start();
 })();
--- resources/js/sticky-header/sticky.js
+++ resources/js/sticky-header/sticky.js
@@ -2,30 +2,38 @@
     /**
      * @returns {number}
      */
-    const getDistance = () => {
-        let topDist = subHeader.offsetTop; // height 33px top-bar
-        return topDist;
+    const getDistance = (el) => {
+        return el.offsetTop; // height 33px top-bar
     }
 
     const subHeader = document.querySelector('.sub-header');
     const logoContainer = document.querySelector('.sub-header__logo-container');
+    const homepageTopMenu = document.querySelector('.homepage-top-grid__menu');
+
     let stuck = false; //
-    const stickPoint = getDistance();
+    const stickPoint = getDistance(subHeader);
+
+    /**
+     * Stick menu large button
+     * @param item
+     */
+    const menuLargeButton = (item) => {
+        (item <= 0) ? logoContainer.classList.add('large-btn') :
+            logoContainer.classList.remove('large-btn');
+    }
 
     const getStuck = () => {
-        const distance = getDistance() - window.pageYOffset;
+        const distance = getDistance(subHeader) - window.pageYOffset;
         const offset = window.pageYOffset;
-
+        const distanceMenu = (homepageTopMenu.offsetHeight - stickPoint) - window.pageYOffset;
         if ((distance <= 0) && !stuck) {
-            subHeader.style.position = 'fixed';
-            logoContainer.classList.add('large-btn');
-            subHeader.style.top = '0px';
+            subHeader.setAttribute("style", "position: fixed; top: 0 ;");
             stuck = true;
         } else if (stuck && (offset <= stickPoint)) {
             subHeader.style.position = 'static';
-            logoContainer.classList.remove('large-btn');
             stuck = false;
         }
+        menuLargeButton(distanceMenu);
     }
     window.onload = getStuck;
     window.onscroll = getStuck;

a1b6a9c181ee3dbea51163ab503349166c96658a
Boris1979509 <kasabian1979509>
2020-07-13 17:57:51 +0300

    Cabinet Home

--- resources/views/cabinet/orders/index.blade.php
+++ resources/views/cabinet/orders/index.blade.php
@@ -18,45 +18,43 @@
                 <div class="content-block__title-prop">{{ __('CabinetStatusOrder') }}</div>
                 <div class="content-block__title-value">Ожидает оплаты</div>
             </div>
+            <div class="content-block__item">
+                <button class="btn btn-active ">{{ __('GoOn') }}</button>
+            </div>
             <span class="accordion__button-item"></span>
             <div class="content-block__sub-content">
                 <div class="content-block__sub-content-item">
                     <div class="content-block__item">
-                        <div class="content-block__title-prop">{{ __('CabinetNameOfGoods') }}</div>
-                        <div class="content-block__title-value">1 Брус 40х50х3000 мм, 1 сорт, септ. г.Вологда Брус
-                            40х50х3000
-                        </div>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th class="content-block__title-prop">{{ __('CabinetNameOfGoods') }}</th>
+                                <th class="content-block__title-prop">{{ __('CabinetPricePerPiece') }}</th>
+                                <th class="content-block__title-prop">{{ __('CabinetOrderQty') }}</th>
+                                <th class="content-block__title-prop">{{ __('CabinetTotalPrice') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="content-block__title-value">1 Брус 40х50х3000 мм, 1 сорт, септ. г.Вологда
+                                    Брус 40х50х3000
+                                </td>
+                                <td class="content-block__title-value total">1 500 ₽</td>
+                                <td class="content-block__title-value total">1 500 ₽</td>
+                                <td class="content-block__title-value total">1 500 ₽</td>
+                            </tr>
+                            <tr>
+                                <td class="content-block__title-value">2 Брус 40х50х3000 мм, 1 сорт, септ. г.Вологда
+                                    Брус 40х50х3000
+                                </td>
+                                <td class="content-block__title-value total">1 500 ₽</td>
+                                <td class="content-block__title-value total">1 500 ₽</td>
+                                <td class="content-block__title-value total">1 500 ₽</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="cabinet-orders__item">
-        <div class="content-block">
-            <div class="content-block__item">
-                <div class="content-block__title-prop">{{ __('CabinetNumberOrder') }}</div>
-                <div class="content-block__title-value">№033400</div>
-            </div>
-            <div class="content-block__item">
-                <div class="content-block__title-prop">{{ __('CabinetDataCreatedOrder') }}</div>
-                <div class="content-block__title-value">19 февраля 2020</div>
-            </div>
-            <div class="content-block__item">
-                <div class="content-block__title-prop">{{ __('CabinetTotalItem') }}</div>
-                <div class="content-block__title-value total">1 300 340 ₽</div>
-            </div>
-            <div class="content-block__item">
-            </div>
-            <span class="accordion__button-item"></span>
-            <div class="content-block__sub-content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto aspernatur aut autem neque
-                nobis
-                sint
-                tenetur! Aperiam dolorem error eveniet fugit illum, ipsam iste labore, minima natus nesciunt nostrum
-                numquam
-                possimus quaerat repellat velit veritatis voluptas. Ab dolores explicabo illum molestiae nesciunt nobis,
-                odio
-                praesentium qui quisquam rem sit.
             </div>
         </div>
     </div>
